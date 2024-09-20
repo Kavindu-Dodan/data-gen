fa6f4bc314a41bac47da7770f8a5d9dbe4607837
Kavindu Dodanduwa <kavindu.dodanduwa@elastic.co>
2024-09-20 09:46:20 -0700

    add firehose log push
    Signed-off-by: Kavindu Dodanduwa <kavindu.dodanduwa@elastic.co>

--- go.mod
+++ go.mod
@@ -3,10 +3,30 @@ module kLog
 go 1.23.0
 
 require (
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.27.35
+	github.com/aws/aws-sdk-go-v2/service/firehose v1.32.5
+	github.com/google/uuid v1.6.0
+	github.com/natefinch/lumberjack v2.0.0+incompatible
+	go.elastic.co/ecszap v1.0.3
+	go.uber.org/zap v1.27.0
+)
+
+require (
+	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.30.5 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.33 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.13 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.17 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.17 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.19 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.22.8 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.26.8 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.30.8 // indirect
+	github.com/aws/smithy-go v1.20.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	go.elastic.co/ecszap v1.0.3 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	go.uber.org/zap v1.27.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log/slog"
@@ -9,32 +10,46 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/firehose"
+	"github.com/aws/aws-sdk-go-v2/service/firehose/types"
 	"github.com/natefinch/lumberjack"
 	"go.elastic.co/ecszap"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
+const (
+	region             = "us-east-1"
+	firehoseStreamName = ""
+	outFirehose        = "FIREHOSE"
+	outLogs            = "LOGS"
+)
+
+type cfg struct {
+	LogOutput      string `json:"output,logOutput"`
+	Processors     int    `json:"processors,omitempty"`
+	LogDelay       int    `json:"log_delay,omitempty"`
+	LogLocation    string `json:"log_location,omitempty"`
+	MaxLogFileSize int    `json:"max_log_file_size,omitempty"`
+}
+
+// NOTE -  current configurations
 var (
-	processors       = 2
+	output           = outFirehose
+	processors       = 1
 	delay            = 1
 	location         = "./logs"
 	maxLogFileSizeMb = 10
 
-	currentConfig = config{
+	currentConfig = cfg{
+		LogOutput:      output,
 		Processors:     processors,
 		LogDelay:       delay,
 		LogLocation:    location,
 		MaxLogFileSize: maxLogFileSizeMb,
 	}
 )
-
-type config struct {
-	Processors     int    `json:"processors,omitempty"`
-	LogDelay       int    `json:"log_delay,omitempty"`
-	LogLocation    string `json:"log_location,omitempty"`
-	MaxLogFileSize int    `json:"max_log_file_size,omitempty"`
-}
 
 func main() {
 	marshal, err := json.Marshal(currentConfig)
@@ -43,29 +58,104 @@ func main() {
 		return
 	}
 
-	slog.Info(fmt.Sprintf("Starting with configurations: %v", string(marshal)))
-
-	// logs to file with rotation
-	lumberJack := zapcore.AddSync(&lumberjack.Logger{
-		Filename: location,
-		MaxSize:  maxLogFileSizeMb,
-	})
-
-	encoderConfig := ecszap.NewDefaultEncoderConfig()
-	core := ecszap.NewCore(encoderConfig, lumberJack, zap.DebugLevel)
-	logger := zap.New(core, zap.AddCaller())
-
-	runner := newRunner(logger)
-	runner.start(processors, time.Duration(delay)*time.Second)
-
+	shutdownChan := make(chan interface{})
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+
+	slog.Info(fmt.Sprintf("Starting with configurations: %v", string(marshal)))
+
+	// choose log writer
+	var sync zapcore.WriteSyncer
+	switch currentConfig.LogOutput {
+	case outLogs:
+		// logs to file with rotation
+		sync = zapcore.AddSync(&lumberjack.Logger{
+			Filename: location,
+			MaxSize:  maxLogFileSizeMb,
+		})
+	case outFirehose:
+		// firehose emitter
+		gCtx := context.Background()
+		cfg, err := config.LoadDefaultConfig(gCtx, config.WithSharedConfigProfile("ecdev"), config.WithRegion(region))
+		if err != nil {
+			slog.Error("error loading default configurations: ", err)
+			return
+		}
+
+		fhClient := firehose.New(firehose.Options{
+			Credentials: cfg.Credentials,
+			Region:      region,
+		})
+		comChan := make(chan []byte, 1)
+		writer := newFireHoseWriter(firehoseStreamName, fhClient, comChan, shutdownChan)
+		sync = zapcore.AddSync(writer)
+	default:
+		slog.Error(fmt.Sprintf("Log output '%s' is not supported", currentConfig.LogOutput))
+		return
+	}
+
+	// elastic encoder
+	encoderConfig := ecszap.NewDefaultEncoderConfig()
+	core := ecszap.NewCore(encoderConfig, sync, zap.DebugLevel)
+
+	logger := zap.New(core, zap.AddCaller())
+	runner := newRunner(logger)
+	runner.start(processors, time.Duration(delay)*time.Second)
 
 	select {
 	case <-sigs:
 		runner.end()
+		close(shutdownChan)
 		<-time.After(1 * time.Second)
 	}
 
 	slog.Info("Exiting")
+}
+
+type fireHoseWriter struct {
+	client     *firehose.Client
+	streamName string
+	listener   chan []byte
+	stopChan   chan interface{}
+}
+
+func newFireHoseWriter(streamName string, client *firehose.Client, listener chan []byte, stopChan chan interface{}) *fireHoseWriter {
+	f := &fireHoseWriter{streamName: streamName, client: client, listener: listener, stopChan: stopChan}
+	f.startFireHoseEmitter()
+
+	return f
+}
+
+func (w fireHoseWriter) Write(p []byte) (n int, err error) {
+	w.listener <- p
+
+	return len(p), nil
+}
+
+func (w fireHoseWriter) startFireHoseEmitter() {
+	// run in background
+	go func() {
+		for {
+			select {
+			case <-w.stopChan:
+				// exit
+				return
+			case line := <-w.listener:
+				record := types.Record{
+					Data: line,
+				}
+
+				putRecord := firehose.PutRecordInput{
+					DeliveryStreamName: &w.streamName,
+					Record:             &record,
+				}
+
+				_, err := w.client.PutRecord(context.Background(), &putRecord)
+				if err != nil {
+					slog.Error("error from firehose put record: ", err)
+					return
+				}
+			}
+		}
+	}()
 }
