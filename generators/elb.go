53663cfe21c0c65402d127d13a38574f67d724e3
Kavindu Dodanduwa <kavindu.dodanduwa@elastic.co>
2025-10-06 10:23:31 -0700

    fix test
    Signed-off-by: Kavindu Dodanduwa <kavindu.dodanduwa@elastic.co>

--- generators/elb.go
+++ generators/elb.go
@@ -19,18 +19,28 @@ func (a *ALBGen) Generate() (int64, error) {
 	accountID := randomSampleAccountID()
 
 	customizer := albCustomizer{
-		logType:        randomSchema(),
-		timestamp:      iso8601Now(),
-		creationTime:   iso8601Now(),
-		clientIPPort:   fmt.Sprintf("%s:%d", randomIP(), randomPort()),
-		targetIPPort:   fmt.Sprintf("%s:%d", randomIP(), randomPort()),
-		targetPortList: fmt.Sprintf("%s:%d", randomIP(), randomPort()),
-		request:        fmt.Sprintf("\"GET %s://%s:80/ HTTP/1.1\"", randomSchema(), randomDomain()),
-		elbID:          fmt.Sprintf("appA/loadbalancer/%s", accountID),
-		targetARN:      fmt.Sprintf("arn:aws:elasticloadbalancing:%s:%s:targetID", randomRegion(), accountID),
-		traceID:        fmt.Sprintf("\"trace=%d\"", rand.Intn(1000)),
-		userAgent:      fmt.Sprintf("\"%s\"", userAgents[rand.Intn(len(userAgents))]),
-		requestID:      randomAZ09String(5),
+		cipher:           randomSSLCipher(),
+		clientIPPort:     fmt.Sprintf("%s:%d", randomIP(), randomPort()),
+		creationTime:     iso8601Now(),
+		domain:           randomDomain(),
+		elbID:            fmt.Sprintf("appA/loadbalancer/%s", accountID),
+		elbStatus:        randomStatus(),
+		logType:          randomSchema(),
+		receivedBytes:    randomBytesSize(),
+		request:          fmt.Sprintf("\"GET %s://%s:80/ HTTP/1.1\"", randomSchema(), randomDomain()),
+		requestID:        randomAZ09String(5),
+		requestProcTime:  randomProcessingTime(),
+		responseProcTime: randomProcessingTime(),
+		sentBytes:        randomBytesSize(),
+		sslProtocol:      sslProtocol(),
+		targetARN:        fmt.Sprintf("arn:aws:elasticloadbalancing:%s:%s:targetID", randomRegion(), accountID),
+		targetIPPort:     fmt.Sprintf("%s:%d", randomIP(), randomPort()),
+		targetPortList:   fmt.Sprintf("%s:%d", randomIP(), randomPort()),
+		targetProcTime:   randomProcessingTime(),
+		targetStatus:     randomStatus(),
+		timestamp:        iso8601Now(),
+		traceID:          fmt.Sprintf("\"trace=%d\"", rand.Intn(1000)),
+		userAgent:        fmt.Sprintf("\"%s\"", userAgents[rand.Intn(len(userAgents))]),
 	}
 
 	err := a.buf.write([]byte(buildALBLogLine(customizer)))
@@ -59,32 +69,42 @@ const (
 )
 
 type albCustomizer struct {
-	logType        string
-	timestamp      string
-	creationTime   string
-	request        string
-	elbID          string
-	targetARN      string
-	traceID        string
-	userAgent      string
-	clientIPPort   string
-	targetIPPort   string
-	targetPortList string
-	requestID      string
+	cipher           string
+	clientIPPort     string
+	creationTime     string
+	domain           string
+	elbID            string
+	elbStatus        string
+	logType          string
+	receivedBytes    int
+	request          string
+	requestID        string
+	requestProcTime  float32
+	responseProcTime float32
+	sentBytes        int
+	sslProtocol      string
+	targetARN        string
+	targetIPPort     string
+	targetPortList   string
+	targetProcTime   float32
+	targetStatus     string
+	timestamp        string
+	traceID          string
+	userAgent        string
 }
 
 func buildALBLogLine(input albCustomizer) string {
 	logLine := fmt.Sprintf(
-		"%s %s %s %s %s %f %f %f %s %s %d %d %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s\n",
+		"%s %s %s %s %s %0.3f %0.3f %0.3f %s %s %d %d %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s\n",
 		input.logType, input.timestamp, input.elbID,
 		input.clientIPPort, input.targetIPPort,
-		randomProcessingTime(), randomProcessingTime(), randomProcessingTime(),
-		randomStatus(), randomStatus(),
-		randomBytesSize(), randomBytesSize(),
+		input.requestProcTime, input.targetProcTime, input.responseProcTime,
+		input.elbStatus, input.targetStatus,
+		input.receivedBytes, input.sentBytes,
 		input.request, input.userAgent,
-		randomSSLCipher(), sslProtocol(),
+		input.cipher, input.sslProtocol,
 		input.targetARN, input.traceID,
-		randomDomain(), chosenCertArn,
+		input.domain, chosenCertArn,
 		matchedRulePriority, input.creationTime,
 		actionsExecuted, redirectURL, errorReason,
 		input.targetPortList, targetStatusList,
