dbefdf3cc36f27379c2654bc9b6f48037c4b9a5b
Kaiyan White <kaiyan.sheng@elastic.co>
2026-01-07 12:48:21 -0700

    chore: Improve log format (#98)
    * Improve log format
    
    * add date back into timestamp and remove printf

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log/slog"
 	"os"
 	"os/signal"
@@ -24,13 +23,13 @@ func main() {
 
 	b, err := os.ReadFile(*cfgLocation)
 	if err != nil {
-		slog.Error(fmt.Sprintf("Config file reading error: %s", err.Error()))
+		slog.Error("Config file reading error", "error", err, "file", *cfgLocation)
 		return
 	}
 
 	configurations, err := conf.NewConfig(b)
 	if err != nil {
-		slog.Error(fmt.Sprintf("Config file parsing error: %s", err.Error()))
+		slog.Error("Config file parsing error", "error", err)
 		return
 	}
 
@@ -39,30 +38,47 @@ func main() {
 		logLevel = slog.LevelDebug
 	}
 
-	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		Level: logLevel,
-	})))
+	// Use a cleaner text handler with better formatting
+	opts := &slog.HandlerOptions{
+		Level:     logLevel,
+		AddSource: false,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.TimeKey {
+				t := a.Value.Time()
+				return slog.String(slog.TimeKey, t.Format("2006-01-02T15:04:05"))
+			}
+			return a
+		},
+	}
 
-	slog.Info(fmt.Sprintf("Starting with configurations: %v", configurations.Print()))
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, opts)))
+
+	slog.Info("Starting data generator")
+	slog.Info("Input", "config", configurations.Input.Print())
+	slog.Info("Output", "config", configurations.Output.Print())
+
+	if configurations.UsesAWS() {
+		slog.Info("AWS", "config", configurations.AWSCfg.Print())
+	}
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
 
 	generator, err := generators.GeneratorFor(configurations)
 	if err != nil {
-		slog.Error(fmt.Sprintf("Error creating generator: %s", err.Error()))
+		slog.Error("Error creating generator", "error", err, "type", configurations.Input.Type)
 		return
 	}
 
 	exporter, err := exporters.ExporterFor(ctx, configurations)
 	if err != nil {
-		slog.Error(fmt.Sprintf("Error creating exporter: %s", err.Error()))
+		slog.Error("Error creating exporter", "error", err, "type", configurations.Output.Type)
 		return
 	}
 
 	duration, err := time.ParseDuration(configurations.Input.Delay)
 	if err != nil {
-		slog.Error(fmt.Sprintf("Error parsing delay: %s, please provide value in acceptable string format like `5s`", err.Error()))
+		slog.Error("Error parsing delay", "error", err, "delay", configurations.Input.Delay, "hint", "use format like '5s' or '10ms'")
 		return
 	}
 
@@ -71,11 +87,13 @@ func main() {
 
 	select {
 	case <-sigs:
+		slog.Info("Received shutdown signal")
 	case <-inputClose:
+		slog.Info("Generator completed")
 	case er := <-genError:
-		slog.Error(fmt.Sprintf("Error from generator: %s", er.Error()))
+		slog.Error("Error from generator", "error", er)
 	case er := <-expErr:
-		slog.Error(fmt.Sprintf("Error from exporter: %s", er.Error()))
+		slog.Error("Error from exporter", "error", er)
 	}
 
 	slog.Info("Shutting down...")
